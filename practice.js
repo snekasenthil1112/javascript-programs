7ceb2996eb1b5786f2da52924546ff566b0598f1
snekasenthil1112 <155375078+snekasenthil1112@users.noreply.github.com>
2024-08-12 22:47:45 +0530

    practice.js

---
+++ practice.js
@@ -0,0 +1,23 @@
+document.write(11+12);
+        window.alert('oooooppppsssss');
+        console.log('hello everyone');
+
+        var a,b,c;
+        a = 10;
+        b = 20;
+        c = a + b;
+        document.getElementById("hi").innerHTML = 
+        "value of c is " + c + ".";
+        document.getElementById("hi").innerHTML = (5 + 6) * 6;
+        
+        document.getElementById("hi").innerHTML = 'sneka';
+        document.getElementById("hi").innerHTML = 12.34;
+
+        document.getElementById("hi").innerHTML = "SNEKA" + " " + "SENHIL";
+        let x;
+        x = 6;
+        document.getElementById("hi").innerHTML = x;
+        let firstName, firstname;
+        firstName = "Sneka";
+        firstname = "Senthil";
+        document.getElementById("hi").innerHTML = firstname; //case sensitive
