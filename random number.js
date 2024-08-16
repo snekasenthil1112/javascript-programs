76e3059d541486f1ed551292ed386f33c4d1af8a
snekasenthil1112 <155375078+snekasenthil1112@users.noreply.github.com>
2024-08-16 19:26:08 +0530

    random number.js

---
+++ random number.js
@@ -0,0 +1,10 @@
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+console.log("Random Number between 1 and 5: ")
+console.log( randomNumber(1, 5) );
+
+/* output
+Random Number between 1 and 5: 
+2.4055867445439976
+*/
