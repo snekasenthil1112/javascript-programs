1d25bccb4938b8593e84e3d16adadae8473c1102
Sneka Senthil <155375078+snekasenthil1112@users.noreply.github.com>
2024-08-23 19:17:05 +0530

    if_else.js

---
+++ if_else.js
@@ -0,0 +1,12 @@
+let score = 45;
+if (score >= 50) {
+    console.log("passed the examination.");
+}
+else {
+    console.log("failed the examination.");
+}
+
+/*
+output
+failed the examination.
+*/
